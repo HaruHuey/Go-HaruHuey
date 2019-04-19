70dc793bca0a0ba7e8f6e040f0bf627260822130
KiHyun Kim <kkh555999@gmail.com>
2019-04-19 20:48:34 +0900

    Type Variable

--- HelloGo.go
+++ HelloGo.go
@@ -1,7 +1,33 @@
-package HaruHuey
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Printf("Hello, Go!\nGo!")
+
+	// int / 8 / 16 / 32 / 64
+	intdata := 7
+
+	// float / 32 / 64
+	var floatdata float32 = 2.8
+
+	stringdata := "HaruHuey Go"
+
+	// int 형 리스트
+	intlist := []int{57, 23, 15}
+
+	stringlist := []string{
+		"HaruHuey",
+		"TypeVariable",
+		"Go lang",
+	}
+
+	fmt.Println(intdata)
+	fmt.Println(floatdata)
+	fmt.Println(stringdata)
+	fmt.Println(intlist[1])
+	fmt.Println(intlist[0], intlist[1], intlist[2])
+	fmt.Println(stringlist[0], stringlist[1], stringlist[2])
 }
